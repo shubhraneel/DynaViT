6a95508f19c0c9e2c8bad8000bdfe7bc6b023d9e
shubhraneel <shubhraneel@gmail.com>
2021-09-16 17:21:41 +0530

    .

--- code/imagenet200_full.py
+++ code/imagenet200_full.py
@@ -30,14 +30,14 @@ val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=64)
 
 train(
     train_loader, val_loader, mode='full', # method='incremental', width_list = [0.2, 0.4, 0.6, 0.8, 1],
-    image_size=64, patch_size=16, num_classes=200, dim=768, dim_head=64, heads=12,
-    depth=12, mlp_dim=3072, dropout=0.1, emb_dropout=0.1, channels=3, pool='cls',
+    image_size=64, patch_size=16, num_classes=200, dim=512, dim_head=64, heads=8,
+    depth=6, mlp_dim=1024, dropout=0.1, emb_dropout=0.1, channels=3, pool='cls',
     epochs=40, loss_fn=nn.CrossEntropyLoss(), model_path="../models/imagenet200"
     )
 
 model = DynaViT(
-            image_size=64, patch_size=16, num_classes=200, dim=768, dim_head=64, heads=12,
-            depth=12, mlp_dim=3072, dropout=0.1, emb_dropout=0.1, channels=3, pool='cls',
+            image_size=64, patch_size=16, num_classes=200, dim=512, dim_head=64, heads=8,
+            depth=6, mlp_dim=1024, dropout=0.1, emb_dropout=0.1, channels=3, pool='cls',
         )
 model.load_state_dict(torch.load("../models/imagenet200/model.pt"))
 
---
+++ code/imagenet200_full_test.py
@@ -0,0 +1,27 @@
+from dynavit import DynaViT, train, print_metrics
+
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from sklearn.metrics import accuracy_score
+
+from torch.utils.data import RandomSampler, DataLoader, SequentialSampler
+import torch
+import torch.nn as nn
+import sys
+
+val_dataset = ImageFolder(path_val, transform=transforms.ToTensor())
+val_sampler = SequentialSampler(val_dataset)
+val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=64)
+
+model = DynaViT(
+            image_size=64, patch_size=16, num_classes=200, dim=512, dim_head=64, heads=8,
+            depth=6, mlp_dim=1024, dropout=0.1, emb_dropout=0.1, channels=3, pool='cls',
+        )
+model.load_state_dict(torch.load("../models/imagenet200/model.pt"))
+
+print_metrics(
+    model, val_loader,
+    [
+     (accuracy_score, {}),
+      ],
+    )
