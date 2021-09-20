242818443bae1a2aa6f756ffdd9f2ab8f192d206
Shubhraneel Pal <shubhraneelpal@gmail.com>
2021-09-20 18:20:56 +0530

    .

--- code/imagenet_fullsized.py
+++ code/imagenet_fullsized.py
@@ -6,9 +6,9 @@ alt_path = "/home/adaq/workspace/SlimmableViT/DynaViT/data/tiny-imagenet-200/"
 dst_path = "/home/adaq/workspace/SlimmableViT/DynaViT/data/ImageNet200FullSize/"
 
 for folder in os.listdir(os.path.join(alt_path, "train")):
+    os.makedirs(os.path.join(dst_path, "train/", folder))
     for f in os.listdir(os.path.join(src_path, "IMAGENET_TRAIN/", folder))[:500]:
-        copyfile(os.path.join(src_path, "train/", folder, f))
-
-for folder in os.listdir(os.path.join(alt_path, "val")):
-    for f in os.listdir(os.path.join(src_path, "IMAGENET_VAL/", folder))[:500]:
-        copyfile(os.path.join(src_path, "val/", folder, f))
+        copyfile(os.path.join(src_path, "IMAGENET_TRAIN/", folder, f), os.path.join(dst_path, "train/", folder, f))
+    os.makedirs(os.path.join(dst_path, "val/", folder))
+    for f in os.listdir(os.path.join(src_path, "IMAGENET_VAL/", folder))[:50]:
+        copyfile(os.path.join(src_path, "IMAGENET_VAL/", folder, f), os.path.join(dst_path, "val/", folder, f))
