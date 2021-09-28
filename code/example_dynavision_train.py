fc91dc8354c7545f8ce66bb2dfd9ee0c7274c357
Shubhraneel Pal <shubhraneelpal@gmail.com>
2021-09-28 10:34:02 +0530

    .

--- code/example_dynavision_train.py
+++ code/example_dynavision_train.py
@@ -40,7 +40,7 @@ test_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=64)
 
 train(train_loader, test_loader, mode="width", method="naive", 
     model_path="../models/", epochs=100, img_size=224, patch_size=16, num_classes=200, 
-    embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,  in_chans = 3, drop_rate=0, attn_drop_rate=0, drop_path_rate=0, distilled=False, act_layer=None, representation_size=None, width_list = [0.2, 0.4, 0.6, 0.8, 1],
+    embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,  in_chans = 3, drop_rate=0, attn_drop_rate=0, drop_path_rate=0, distilled=False, act_layer=None, representation_size=None, width_list = [0.2, 0.4, 0.6, 0.8, 1], loss_fn=nn.CrossEntropyLoss(),
     qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6)
 )
 
