240efd4193235395ed3b49f27326db1656768f2a
Shubhraneel Pal <shubhraneelpal@gmail.com>
2021-10-01 11:16:55 +0530

    .

--- code/print_deit.py
+++ code/print_deit.py
@@ -32,4 +32,4 @@ else:
 model = VisionTransformer(
         patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6))
-count_parameters(model)
+print(count_parameters(model))
